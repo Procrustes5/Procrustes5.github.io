e3f564b64f64d07edaa08538711bfa4e8a6a2a19
Hanse Kim <procrustes5@gmail.com>
2023-02-06 22:11:53 +0900

    230206 To Dos

--- js/todo.js
+++ js/todo.js
@@ -2,10 +2,25 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
 
+const toDos = [];
+
+function saveToDos () {
+    localStorage.setItem("todos", JSON.stringify(toDos));
+}
+
+function deleteToDo (event) {
+    const li = event.target.parentElement;
+    li.remove();
+}
+
 function paintToDo (newToDo) {
     const li = document.createElement("li");
     const span = document.createElement("span");
+    const button = document.createElement("button");
+    button.innerText = "❌";
     li.appendChild(span);
+    li.appendChild(button);
+    button.addEventListener("click", deleteToDo);
     span.innerText = newToDo;
     toDoList.appendChild(li);
 }
@@ -14,7 +29,9 @@ function handleToDoSubmit (event) {
     event.preventDefault();
     const newToDo = toDoInput.value;
     toDoInput.value = "";
+    toDos.push(newToDo);
     paintToDo(newToDo);
+    saveToDos();
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
